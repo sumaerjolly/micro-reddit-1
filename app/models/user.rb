280c1cea0dd395bae842477191ead797bc316b02
Sumaer Jolly <sumaerjolly@gmail.com>
2019-07-22 19:45:13 +0530

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, length: { minimum: 6 } , presence: true
+    validates :email, presence: true , format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
