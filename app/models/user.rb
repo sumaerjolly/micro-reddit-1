9239cab9cd7f0141d8c1297cd48a71128dc830a7
Sumaer Jolly <sumaerjolly@gmail.com>
2019-07-23 15:01:41 +0530

    All models added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
     validates :username, length: { minimum: 6 } , presence: true
     validates :email, presence: true , format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
